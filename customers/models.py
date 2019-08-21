b992e4ed985ab9644880247d405658e2272ccd58
ashraful88 <aaj.cse@gmail.com>
2019-08-21 19:05:46 +0800

    customer model

--- customers/models.py
+++ customers/models.py
@@ -16,3 +16,15 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 #todo: add contact model to add multiple contacts of a customer
+
+class CustomerContact(models.Model):
+    id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, unique=True)
+    phone = models.CharField(max_length=200, unique=True)
+    customer = models.ForeignKey(Customer)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+    
