89c79e64373885672986a91161c20ba162e9807b
ashraful88 <aaj.cse@gmail.com>
2020-02-27 19:00:22 +0800

    apps

--- new/tidebilling/customers/admin.py
+++ new/tidebilling/customers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer
+
+class CustomerAdmin(admin.ModelAdmin):
+	list_display = ("id", "name", "email", "phone")
+	#prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Customer, CustomerAdmin)
--- new/tidebilling/customers/models.py
+++ new/tidebilling/customers/models.py
@@ -6,14 +6,27 @@ import uuid
 
 class Customer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    cus_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200, unique=True)
     phone = models.CharField(max_length=200)
     note = models.TextField()
-    slug = models.SlugField(max_length=200, unique=True)
+    #slug = models.SlugField(max_length=200, unique=True)
     status = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
-#todo: add contact model to add multiple contacts of a customer
+
+
+class CustomerContact(models.Model):
+    id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, unique=True)
+    phone = models.CharField(max_length=200, unique=True)
+    customer = models.ForeignKey(Customer)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+    
